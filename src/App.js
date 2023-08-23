6045e9c8f7854e1c3b5742c1e7cac3dc70522dd3
tech-teni <yakubganiyat96@gmail.com>
2023-08-23 15:35:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <BrowserRouter >
     <Routes>
       <Route  path='/'  element={<HomePage/>}/>
+     <Route  path='/view-product/:id'  element={<ViewProduct/>}/> 
 
     </Routes>
     </BrowserRouter>
--- src/pages/ViewProduct.js
+++ src/pages/ViewProduct.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import Navbar from '../components/Navbar'
 import Products from '../components/Products';
  import { useParams } from 'react-router-dom';
-
+import productList from '../API';
 
 
 
 const ViewProduct = () => {
+
+    const[data, setData]=useState({})
+
+    const paramsId =useParams()
+    console.log(paramsId.id)
    
+
+    useEffect(()=>{
+       let result= productList.filter((each)=>{
+
+            return each.id === Number(paramsId.id)
+        })
+
+        setData(result[0])
+
+    }, [])
   return (
     <div>
      <Navbar/>
-     <Products/>   
+     <div className='product view-product' key={data.id}>
+          <div><h5>{data.name}</h5>
+            <p>Type: {data.type}</p>
+            <p>Description: {data.description}</p></div>
+          <div><button className={data.status}>{data.status}</button>
+          <p>{data.date_created}</p></div>
+      
+          </div> 
     </div>
   )
 }
